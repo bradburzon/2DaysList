54e0cf2ba3f63b6b1b8f5f497d4b7af02177c7b2
Brad Burzon <bradburzon@ymail.com>
2024-02-17 20:37:14 -0800

    Add ability to add task in TaskManager

--- app/src/main/java/com/bradburzon/a2dayslist/TaskManager.java
+++ app/src/main/java/com/bradburzon/a2dayslist/TaskManager.java
@@ -1,5 +1,7 @@
 package com.bradburzon.a2dayslist;
 
+import java.util.UUID;
+
 public class TaskManager {
 
     private TaskStorage storage;
@@ -10,5 +12,10 @@ public class TaskManager {
 
     public void markAsComplete(String taskId) {
         storage.getById(taskId).setTaskStatus(TaskStatus.COMPLETED);
+    }
+
+    public void addTask(String taskName) {
+        String taskId = UUID.randomUUID().toString();
+        storage.add(new Task(taskId, taskName, storage.list().size(), TaskStatus.CREATED));
     }
 }
