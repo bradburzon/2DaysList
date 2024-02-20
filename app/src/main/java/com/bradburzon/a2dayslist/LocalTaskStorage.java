40f46b0551e300f65974692b99b796208da4aa0e
Brad Burzon <bradburzon@ymail.com>
2024-02-19 17:42:43 -0800

    Implement delete and sort. Add Comparator<Task> for name.

--- app/src/main/java/com/bradburzon/a2dayslist/LocalTaskStorage.java
+++ app/src/main/java/com/bradburzon/a2dayslist/LocalTaskStorage.java
@@ -1,6 +1,9 @@
 package com.bradburzon.a2dayslist;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -18,8 +21,8 @@ public class LocalTaskStorage implements TaskStorage {
     }
 
     @Override
-    public void delete(String taskId) {
-        data.remove(taskId);
+    public Task delete(String taskId) {
+        return data.remove(taskId);
     }
 
     @Override
@@ -35,5 +38,20 @@ public class LocalTaskStorage implements TaskStorage {
     @Override
     public List<Task> list() {
         return new ArrayList<Task>(data.values());
+    }
+
+    @Override
+    public void sort(Comparator taskComparator) {
+        List<Task> sortedTasks = new ArrayList<>(data.values());
+        System.out.println(sortedTasks);
+        Collections.sort(sortedTasks, taskComparator);
+        System.out.println(sortedTasks);
+        HashMap<String, Task> sortedData = new HashMap<>();
+        int i = 0;
+        for(Task task : sortedTasks){
+            sortedData.put(i + "", task);
+            i++;
+        }
+        data = sortedData;
     }
 }
--- app/src/test/java/com/bradburzon/a2dayslist/LocalTaskStorageTest.java
+++ app/src/test/java/com/bradburzon/a2dayslist/LocalTaskStorageTest.java
@@ -1,13 +1,14 @@
 package com.bradburzon.a2dayslist;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 import org.junit.Before;
 import org.junit.Test;
+
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.Map;
 import java.util.List;
+import java.util.Map;
 
 public class LocalTaskStorageTest {
     private Map<String, Task> startingData;
@@ -41,10 +42,11 @@ public class LocalTaskStorageTest {
     public void givenNonEmptyStartingDataWhenDeleteIsCalledThenRemoveTaskFromStorage() {
         TaskStorage storage = new LocalTaskStorage(startingData);
 
-        storage.delete("1");
+        Task actual = storage.delete("1");
         List<Task> list = storage.list();
 
-        assertEquals(new ArrayList<>(), list);
+        assertEquals(0, list.size());
+        assertEquals( new Task("1", "name", 1, TaskStatus.CREATED), actual);
     }
 
     @Test
@@ -64,5 +66,17 @@ public class LocalTaskStorageTest {
         Task actual = storage.getById("1");
 
         assertEquals( new Task("1", "updatedName", 1, TaskStatus.CREATED), actual);
+    }
+
+    @Test
+    public void givenNonEmptyStartingDataWhenSortIsCalledThenSortTheData() {
+        TaskStorage storage = new LocalTaskStorage(startingData);
+        storage.add(new Task("1", "Zame", 1, TaskStatus.CREATED));
+        storage.add(new Task("2", "Name", 2, TaskStatus.CREATED));
+
+        storage.sort(new TaskSorter.NameSort());
+        Task actual = storage.list().get(0);
+
+        assertEquals( new Task("2", "Name", 2, TaskStatus.CREATED), actual);
     }
 }
