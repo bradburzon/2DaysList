3977f9b361393976c8a6e34473d21472e8b5dbd5
Brad Burzon <bradburzon@ymail.com>
2024-02-19 17:40:10 -0800

    Add delete and sort to TaskStorage interface

--- app/src/main/java/com/bradburzon/a2dayslist/TaskStorage.java
+++ app/src/main/java/com/bradburzon/a2dayslist/TaskStorage.java
@@ -1,12 +1,14 @@
 package com.bradburzon.a2dayslist;
 
+import java.util.Comparator;
 import java.util.List;
 
 public interface TaskStorage {
     void add(Task task);
-    void delete(String taskId);
+    Task delete(String taskId);
     void update(String taskId, Task task);
     Task getById(String taskId);
     List<Task> list();
+    void sort(Comparator taskComparator);
 }
 
