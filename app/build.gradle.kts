24fc98558c69605be90aa5d03e0718eac7f0688a
Brad Burzon <bradburzon@ymail.com>
2024-04-01 16:47:38 -0700

    Add Espresso to gradle build

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -52,6 +52,12 @@ dependencies {
     androidTestImplementation ("androidx.test.ext:junit:1.1.5")
     // Android Instrumented Tests
     androidTestImplementation ("androidx.test.espresso:espresso-core:3.5.1")
-    androidTestImplementation( "androidx.test.espresso:espresso-contrib:3.5.1")
+    androidTestImplementation ("androidx.test.espresso:espresso-contrib:3.5.1")
+    // UI
+    implementation ("androidx.test.ext:junit-ktx:1.1.5")
+    androidTestImplementation ("androidx.test.espresso:espresso-intents:3.5.1")
+    implementation ("androidx.test.espresso:espresso-idling-resource:3.5.1")
+    androidTestImplementation ("androidx.test.espresso:espresso-idling-resource:3.5.1")
+    androidTestImplementation ("org.hamcrest:hamcrest:2.2")
 
 }
